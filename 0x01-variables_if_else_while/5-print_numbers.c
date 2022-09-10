c467114f5acfca7bd3f35fca3239bc2eb73f6591
Motabataba <tabane.montwedi@gmail.com>
2022-09-09 17:41:15 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf("%d", i);
+
+if (i == 9)
+printf("\n");
+i++;
+}
+return (0);
+}
