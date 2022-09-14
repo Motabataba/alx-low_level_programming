caca9820155b30f4a1ff1f1e7f738574ef294c0f
Motabataba <tabane.montwedi@gmail.com>
2022-09-14 05:07:51 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints
+ * @c: passed variable
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int c)
+{
+int last_digit;
+if (c < 0)
+c = (-1) * c;
+
+last_digit = c % 10;
+_putchar(last_digit + '0');
+return (last_digit);
+}
