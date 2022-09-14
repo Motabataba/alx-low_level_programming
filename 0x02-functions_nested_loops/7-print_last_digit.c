5a6bfaf433461d5c597d5c517bb1658f75a5e25d
Motabataba <tabane.montwedi@gmail.com>
2022-09-14 06:43:48 -0700

    print last digit_02

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,9 @@
 
 int print_last_digit(int c)
 {
-int last_digit;
-if (c < 0)
-c = (-1) * c;
-last_digit = c % 10;
+int last_digit = c % 10;
+if (last_digit < 0)
+last_digit = (-1) * last_digit;
 _putchar(last_digit + '0');
 return (last_digit);
 }
