acf7a909cf2335ff4705f4ea4a26d66afce4752f
Motabataba <tabane.montwedi@gmail.com>
2022-09-14 06:18:47 -0700

    print last digit_01

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,6 @@ int print_last_digit(int c)
 int last_digit;
 if (c < 0)
 c = (-1) * c;
-
-if (c / 10)
-print_last_digit(c / 10);
-
 last_digit = c % 10;
 _putchar(last_digit + '0');
 return (last_digit);
