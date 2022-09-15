2e05768130280cd3e68e8bf50e0f8b6e0ef95d01
Motabataba <tabane.montwedi@gmail.com>
2022-09-15 11:33:54 -0700

    numbers speak

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers -  print nums
+ *
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+int numb = '0';
+while (numb <= '9')
+{
+_putchar(numb);
+numb++;
+}
+_putchar('\n');
+}
