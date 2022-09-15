6f609d6e575d87d19c99df1745dd0dc76dda67fd
Motabataba <tabane.montwedi@gmail.com>
2022-09-15 13:15:59 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - check code
+ * @n: argument
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i <= n; i++)
+{
+_putchar('-');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
