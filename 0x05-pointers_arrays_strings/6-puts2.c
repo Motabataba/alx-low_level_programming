7d0e7b2d1174f27ccc8354b8893dea074b6318b6
Motabataba <tabane.montwedi@gmail.com>
2022-09-19 12:31:33 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ * @str: agrument
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+str++;
+}
+_putchar('\n');
+}
