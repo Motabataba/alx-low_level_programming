291e353b0f371b50a7d16ce7b78cbf2be62cd799
Motabataba <tabane.montwedi@gmail.com>
2022-09-21 10:09:50 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,24 +10,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int size1 = 0;
-int size2 = 0;
-int val;
 
-while (s1[size1])
-size1++;
+while (*s1 && *s2 && *s1 == *s2)
+{
+s1++;
+s2++;
+}
 
-while (s2[size2])
-size2++;
-
-if (size1 > size2)
-val = 15;
-
-if (size1 == size2)
-val = 0;
-
-if (size1 < size2)
-val = -15;
-
-return (val);
+return (*s1 - *s2);
 }
