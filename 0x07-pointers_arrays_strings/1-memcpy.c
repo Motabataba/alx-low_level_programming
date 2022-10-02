5d87d6260f9d84e3150e828c1e60a84b8f0060a6
Motabataba <tabane.montwedi@gmail.com>
2022-10-02 05:17:43 -0700

    me copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - check code
+ *@dest: to be appended
+ *@src: to be copied from
+ *@n: size
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+while (i < n)
+{
+*dest = src[i];
+dest++;
+i++;
+}
+return (dest);
+}
